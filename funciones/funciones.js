f93e291d0f13c09cab193dbabbe00fcc41f67738
mariano milla <mariano_milla@live.com>
2023-02-24 00:22:37 -0300

    proyecto final cv

---
+++ funciones/funciones.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    document.getElementById('navlink3').addEventListener('click',function(){
+        window.print();
+    });
+});
+
+/*funcion imprimir*/
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    
+    document.getElementById("inicio").addEventListener("click", function(){
+        window.scrollTo(0,-500)
+    });
+});
+/*funcion scroll hacia arriba(header)*/
